893521d447d8cee27947f213b2e052b796dd2202
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-05-11 13:06:34 -0300

    feat: ajustes ser controllers

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -83,7 +83,10 @@ const login = async (req, res, next) => {
     );
     return next(error);
   }
-  res.json({ message: 'Logged in!' });
+  res.json({
+    message: 'Logged in!',
+    user: existingUser.toObject({ getters: true }),
+  });
 };
 
 //EXPORT FEATURES
